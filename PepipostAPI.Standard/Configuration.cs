0c307f417029fefcfd337b5f7d09b37c52aa9735
imvsahu <imvikramsahu@outlook.com>
2018-04-26 13:41:55 +0530

    Official repo of Csharp updated - v2.0.0

---
+++ PepipostAPI.Standard/Configuration.cs
@@ -0,0 +1,11 @@
+using PepipostAPI.Standard.Models;
+namespace PepipostAPI.Standard
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://api.pepipost.com";
+
+    }
+}
---
+++ PepipostAPI.Standard/Utilities/CustomDateTimeConverter.cs
@@ -0,0 +1,12 @@
+using Newtonsoft.Json.Converters;
+
+namespace PepipostAPI.Standard.Utilities
+{
+    public class CustomDateTimeConverter : IsoDateTimeConverter
+    {
+        public CustomDateTimeConverter(string format)
+        {
+            DateTimeFormat = format;
+        }
+    }
+}
