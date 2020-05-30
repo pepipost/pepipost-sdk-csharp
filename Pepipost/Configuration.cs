b1f6f173e403d56817e246065491ad43db57d847
Vikram <snipperbytes@gmail.com>
2020-05-30 14:32:53 +0530

    adding updated lib files

---
+++ Pepipost/Configuration.cs
@@ -0,0 +1,16 @@
+using Pepipost.Models;
+namespace Pepipost
+{
+    public partial class Configuration
+    {
+
+
+        //The base Uri for API calls
+        public static string BaseUri = "https://api.pepipost.com/v5";
+
+        //Your Pepipost API Key. You will find the api key in the Pepipost application in Integrations.
+        //TODO: Replace the ApiKey with an appropriate value
+        public static string ApiKey = "";
+
+    }
+}
